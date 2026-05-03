8739b1a7e89eebba4706eb7701d79dac12a152ef
BadBotExe <>
2026-05-04 01:37:36 +0200

    chore: add versions to fight caching

--- bonuses/app.js
+++ bonuses/app.js
@@ -1354,7 +1354,7 @@ const app = createApp({
         const params = new URLSearchParams(window.location.search);
 
         try {
-            const r = await fetch('bonuses.json');
+            const r = await fetch('bonuses.json?v=1');
             this.data = await r.json();
 
             const sourceArrays = await Promise.all(
